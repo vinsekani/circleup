75ac21db1a93f082f9853cfff149c5167d3a0b96
vinsekani <vinsekani@gmail.com>
2025-03-04 08:00:34 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Router from "./components/Router";
+
+export default function App() {
+  return (
+    <div className="text-[5rem] text-[red]">
+      <Router/>
+    </div>
+  );
+}
---
+++ src/components/Router.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Homepage } from "../pages/Homepage";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            
+              <Homepage />
+            
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
